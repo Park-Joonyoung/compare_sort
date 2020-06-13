694c703b25e74c52052d8586c90317a85f1c1586
Joonyoung Park <sparkjy18@gmail.com>
2020-06-13 19:52:00 +0900

    Thrid commit

--- compare_sort/compare_sort.c
+++ compare_sort/compare_sort.c
@@ -1,3 +1,11 @@
+/* MEMO*/
+/*
+ *  다른 sort 알고리즘 추가하기
+ *
+ *
+ *
+ */
+
 /* Compares given sorting algorithms */
 
 #include <stdio.h>
@@ -5,54 +13,60 @@
 #include <time.h>
 
 #define MAX 50000
-#define BEST_TIME_FORMAT "최선(ms): %3g \n"
-#define AVG_TIME_FORMAT "평균(ms): %3g \n"
-#define WORST_TIME_FORMAT "최악(ms): %3g \n"
+#define BEST_TIME_FORMAT "최선(ms): %g \n"
+#define AVG_TIME_FORMAT "평균(ms): %g \n"
+#define WORST_TIME_FORMAT "최악(ms): %g \n"
 
 typedef struct {
 	double best_time;
 	double avg_time;
 	double worst_time;
-	double memory_used;
 } Result;
 
 /* function prototypes */
 void getvars(int *size, int *repeat);
-void init(Result *bubble_ptr);
+void init(Result *sort_ptr);
 void random(int *arr, int size);
-void print_all(Result bubble);
-void bubble_sort(Result *bubble_ptr, int *arr, int size, int repeat);
+void print_all(Result *sort);
+void do_sort(Result *sort_ptr, int sort_name, int *arr, int size, int repeat);
+/* algorithms */
+void bubble(int *arr, int size);
+void selection(int *arr, int size);
+void insertion(int *arr, int size);
+void quick(int *arr, int size);
+void merge(int *arr, int size);
+void heap(int *arr, int size);
 
 int main(void)
 {
+	enum { BUBBLE, SELECTION, INSERTION, QUICK, MERGE, HEAP };
+	int i = 0;
 	int size, repeat; getvars(&size, &repeat);
 	int arr[MAX];
-	Result bubble;
-	Result *bubble_ptr = &bubble;
+	Result sort[6];
 
-	init(bubble_ptr);
+	for (i = BUBBLE; i <= HEAP; ++i) {
+		init(&sort[i]);
+	}
 
 	srand((int)time(NULL));
 
 	random(arr, size);
 
-	bubble_sort(bubble_ptr, arr, size, repeat);
-	/*
-	quick();
-	insert();
-	...
-	*/
+	for (i = BUBBLE; i <= HEAP; ++i) {
+		do_sort(&sort[i], i, arr, size, repeat);
+	}
 
-	print_all(bubble);
+	print_all(sort);
 
 	return 0;
 }
 
-void init(Result *bubble_ptr)
+void init(Result *sort_ptr)
 {
-	bubble_ptr->best_time = 0.0;
-	bubble_ptr->avg_time = 0.0;
-	bubble_ptr->worst_time = 0.0;
+	sort_ptr->best_time = 0.0;
+	sort_ptr->avg_time = 0.0;
+	sort_ptr->worst_time = 0.0;
 }
 
 void getvars(int *size, int *repeat)
@@ -104,49 +118,104 @@ void random(int *arr, int size)
 	}
 }
 
-void print_all(Result bubble)
+void print_all(Result *sort)
 {
-	printf("Bubble Sort \n");
-	printf(BEST_TIME_FORMAT, bubble.best_time);
-	printf(AVG_TIME_FORMAT, bubble.avg_time);
-	printf(WORST_TIME_FORMAT, bubble.worst_time);
+	enum { BUBBLE, SELECTION, INSERTION, QUICK, MERGE, HEAP };
+	char *sort_name[6] = {"Bubble", "Selection", "Insertion", "Quick", "Merge", "Heap"};
+	int i;
+
+	for (i = BUBBLE; i <= HEAP; ++i) {
+		printf("%s Sort \n", sort_name[i]);
+		printf(BEST_TIME_FORMAT, sort[i].best_time);
+		printf(AVG_TIME_FORMAT, sort[i].avg_time);
+		printf(WORST_TIME_FORMAT, sort[i].worst_time);
+	}
+	
 }
 
-void bubble_sort(Result *bubble_ptr, int *arr, int size, int repeat)
+void do_sort(Result *sort_ptr, int sort_name, int *arr, int size, int repeat)
 {
+	enum { BUBBLE, SELECTION, INSERTION, QUICK, MERGE, HEAP };
 	double t_i, t_f;
 	double time[MAX];							// the array stores time taken during sorting elements
-	int i, j, k, temp;
+	int i;
 	double t_sum = 0.0;
 
 	for (i = 0; i < repeat; ++i) {
 		t_i = clock();
-		/******************************************************************/
-
-		for (j = size - 1; j > 0; --j) {
-			for (k = 0; k < j; ++k) {
-				if (arr[k] > arr[k + 1]) {
-					temp = arr[k];
-					arr[k] = arr[k + 1];
-					arr[k + 1] = temp;
-				}
-			}
+		
+		switch (sort_name) {
+		case BUBBLE:
+			bubble(arr, size);
+			break;
+		case SELECTION:
+			selection(arr, size);
+			break;
+		case INSERTION:
+			insertion(arr, size);
+			break;
+		case QUICK:
+			quick(arr, size);
+			break;
+		case MERGE:
+			merge(arr, size);
+			break;
+		case HEAP:
+			heap(arr, size);
+			break;
+		default:
+			printf("유효하지 않은 알고리즘이 전달되었습니다. \n");
+			exit(-1);
 		}
 
-		/******************************************************************/
 		t_f = clock();
 		time[i] = (double)t_f - t_i;
 	}
 
-	bubble_ptr->best_time = bubble_ptr->worst_time = time[0];
+	sort_ptr->best_time = sort_ptr->worst_time = time[0];
 	for (i = 0; i < repeat; ++i) {
 		t_sum += time[i];
-		bubble_ptr->avg_time = t_sum / (double) repeat;
-		if (bubble_ptr->best_time > time[i]) {
-			bubble_ptr->best_time = time[i];
+		sort_ptr->avg_time = t_sum / (double) repeat;
+		if (sort_ptr->best_time > time[i]) {
+			sort_ptr->best_time = time[i];
 		}
-		if (bubble_ptr->worst_time < time[i]) {
-			bubble_ptr->worst_time = time[i];
+		if (sort_ptr->worst_time < time[i]) {
+			sort_ptr->worst_time = time[i];
 		}
 	}
+}
+
+void bubble(int *arr, int size)
+{
+	int i, j, temp;
+
+	for (i = size - 1; i > 0; --i) {
+		for (j = 0; j < i; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+void selection(int *arr, int size) {
+
+}
+
+void insertion(int *arr, int size) {
+
+}
+
+void quick(int *arr, int size) {
+
+}
+
+void merge(int *arr, int size) {
+
+}
+
+void heap(int *arr, int size) {
+
 }
